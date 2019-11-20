db3b9a3b7b24dafd376215f4d921e0ffac3bb07b
Arman Uddin <uarman2215@gmail.com>
2019-11-20 16:24:23 -0500

    Working on customer profile page

--- src/app/components/customers/profile/profile.component.ts
+++ src/app/components/customers/profile/profile.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from 'src/app/services/customer.service';
+import { take } from 'rxjs/operators';
+import { Customer } from 'src/app/interfaces/customer';
+import { CartItem } from 'src/app/interfaces/cart-item';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit 
+{
+  userUid:string = "";
+  userName:string = "";
+  userPhone:string = "";
+  userEmail:string = "";
+  customerAddress:string = "";
+  customerNameOnCard:string = "";
+  customerCardNumber:string = "";
+  customerCVV:number = 0;
+  customerExpirationDate:Date = null;
+  customerCart:CartItem[] = null;
 
-  constructor() { }
+  constructor(private custServe:CustomerService) 
+  { 
+    this.custServe.getCurrentUser().pipe(take(1)).subscribe(user=>{
+      this.userUid = user.uid;
+      this.userName = user.name;
+      this.userPhone = user.phone;
+      this.userEmail = user.email;
+    })
+
+    this.custServe.getCurrentCustomer().pipe(take(1)).subscribe(customer=>{
+      this.customerAddress = customer.address;
+      this.customerNameOnCard = customer.nameOnCard;
+      this.customerCardNumber = customer.cardNumber;
+      this.customerCVV = customer.CVV;
+      this.customerExpirationDate = customer.expirationDate;
+      this.customerCart = customer.shoppingCart;
+    })
+  }
 
   ngOnInit() {
   }
 
+  update(formValue)
+  {
+    let tempCust:Customer = {
+      uid: this.userUid,
+      address: formValue.address,
+      cardNumber: formValue.cardNumber,
+      CVV: formValue.CVV,
+      nameOnCard: formValue.nameOnCard,
+      expirationDate: formValue.expirationDate as Date,
+      shoppingCart: this.customerCart
+    }
+    this.custServe.updateCustomer(this.userUid,tempCust);
+  }
 }
